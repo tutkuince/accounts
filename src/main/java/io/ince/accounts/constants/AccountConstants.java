4af269209a4e898f1281e36592eb270fd9620e65
Tutku Ince <tutku.ince@outlook.com>
2023-10-23 00:21:40 +0300

    AccountConstants has been created

---
+++ src/main/java/io/ince/accounts/constants/AccountConstants.java
@@ -0,0 +1,17 @@
+package io.ince.accounts.constants;
+
+public class AccountConstants {
+
+    private AccountConstants() {
+    }
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADRESS = "123 Main Street, New York";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_202 = "202";
+    public static final String MESSAGE_202 = "Request processed successfully";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+
+}
