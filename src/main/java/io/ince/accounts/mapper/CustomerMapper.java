76d03e8e70dff900d16ccc591d788aca84d11f28
Tutku Ince <tutku.ince@outlook.com>
2023-10-23 00:22:10 +0300

    Mapper classes have been created

---
+++ src/main/java/io/ince/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package io.ince.accounts.mapper;
+
+import io.ince.accounts.dto.CustomerDto;
+import io.ince.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setName(customer.getName());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setEmail(customerDto.getEmail());
+        customer.setName(customerDto.getName());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
