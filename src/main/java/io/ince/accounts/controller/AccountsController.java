562179980b58ac89abe22c71b7839330b3386045
Tutku Ince <tutku.ince@outlook.com>
2023-10-23 01:27:33 +0300

    AccountsController has been implemented for Create API

--- src/main/java/io/ince/accounts/controller/AccountsController.java
+++ src/main/java/io/ince/accounts/controller/AccountsController.java
@@ -1,12 +1,30 @@
 package io.ince.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.ince.accounts.constants.AccountConstants;
+import io.ince.accounts.dto.CustomerDto;
+import io.ince.accounts.dto.ResponseDto;
+import io.ince.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
 
-    @GetMapping("/sayHello")
-    public String sayHello() {
-        return "Hello World";
+    private IAccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
+
 }
