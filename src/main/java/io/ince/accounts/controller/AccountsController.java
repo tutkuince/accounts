5766ab2441ee082f5c384ab17571c9558efb89fc
Tutku Ince <tutku.ince@outlook.com>
2023-10-23 22:49:27 +0300

    Perform input data validations inside accounts microservice

--- src/main/java/io/ince/accounts/controller/AccountsController.java
+++ src/main/java/io/ince/accounts/controller/AccountsController.java
@@ -4,21 +4,25 @@ import io.ince.accounts.constants.AccountConstants;
 import io.ince.accounts.dto.CustomerDto;
 import io.ince.accounts.dto.ResponseDto;
 import io.ince.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated // This annotation will tell to my Spring Boot Framework to perform validations on all the Rest APIs.
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -26,13 +30,16 @@ public class AccountsController {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(
+            @RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if (isUpdated) {
             return ResponseEntity
@@ -46,7 +53,10 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobilePhone) {
+    public ResponseEntity<ResponseDto> deleteAccountDetails(
+            @RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            String mobilePhone) {
         boolean isDeleted = iAccountsService.deleteAccount(mobilePhone);
         if (isDeleted) {
             return ResponseEntity
--- src/main/java/io/ince/accounts/dto/AccountsDto.java
+++ src/main/java/io/ince/accounts/dto/AccountsDto.java
@@ -1,10 +1,18 @@
 package io.ince.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+    @NotEmpty
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
